7f3aa34a152566d24a73f5e4138a80a376d4be17
narasinhaa <narasinhagaikwad@gmail.com>
2024-11-19 10:16:58 +0530

    js examples
    new

---
+++ flipkart/src/scripts/project.js
@@ -0,0 +1,11 @@
+export function Addition(a,b){
+    return a + b;
+}
+
+export default function Multiplication(a,b) {
+    return a * b;
+}
+
+export function Hello(user) {
+    return `Hello ! ${user}`;
+}
